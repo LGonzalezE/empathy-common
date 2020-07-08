5d46532b7140e46f6399157ee6a7044ad5e9b916
LGonzalezE <leonardo.gonzalez.escobar@gmail.com>
2020-07-08 10:31:47 -0500

    Full version

---
+++ src/main/java/com/empathy/util/IssueUtil.java
@@ -0,0 +1,29 @@
+package com.empathy.util;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+
+public class IssueUtil {
+
+	public static double calculateProgress(Date date1, Date date2) throws Exception {
+		// calculate progress
+		LocalDateTime startDate = DateTime.convertToLocalDateTimeViaSqlTimestamp(date1);
+		LocalDateTime endDate = DateTime.convertToLocalDateTimeViaSqlTimestamp(date2);
+		LocalDateTime now = LocalDateTime.now();
+		long totalDays = startDate.until(endDate, ChronoUnit.DAYS);
+		long elapsedDays = startDate.until(now, ChronoUnit.DAYS);
+
+		if (totalDays == 0 || totalDays < 0) {
+			StringBuilder strBuilder = new StringBuilder("invalid date range: [").append(date1).append(", ")
+					.append(date2).append("]");
+
+			throw new Exception(strBuilder.toString());
+		}
+
+		double progress = ((double) elapsedDays / totalDays) * 100;
+
+		return progress;
+	}
+}
